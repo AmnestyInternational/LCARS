186b930589637531972db30b78044c4a6fd41bdf
gregology <greg@gho.st>
2013-05-21 17:04:59 -0400

    changed start time of a couple of jobs

--- jobs/Twitter_Influential_Users.rb
+++ jobs/Twitter_Influential_Users.rb
@@ -4,7 +4,7 @@ require 'tiny_tds'
  
 yml = YAML::load(File.open('lib/db_settings.yml'))['prod_settings']
 
-SCHEDULER.every '10m', :first_in => 0 do |job|
+SCHEDULER.every '10m', :first_in => 355 do |job|
   tweetusers = []
 
   client = TinyTds::Client.new(:username => yml['username'], :password => yml['password'], :host => yml['host'])
