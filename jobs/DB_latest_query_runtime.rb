22799322daf67d5847b4764015064ec36cb65f1d
gregology <greg@gho.st>
2013-05-23 12:22:38 -0400

    house cleaning

--- jobs/DB_latest_query_runtime.rb
+++ jobs/DB_latest_query_runtime.rb
@@ -4,32 +4,27 @@ require 'tiny_tds'
 
 yml = YAML::load(File.open('lib/db_settings.yml'))['prod_settings']
 
-SCHEDULER.every '1m' do
+SCHEDULER.every '5m', :first_in => 37 do |job|
   lastqueryrun = []
-  # Get SQL Errors
+
   client = TinyTds::Client.new(:username => yml['username'], :password => yml['password'], :host => yml['host'])
   result = client.execute("
     USE externaldata
     SELECT
-     (SELECT MAX(updated) 'latest' FROM fb_link_count) 'fb_link_count',
-     (SELECT MAX(updated_time) 'latest' FROM fb_page_post) 'fb_page_post',
-     (SELECT MAX(updated) 'latest' FROM fb_page_post_stat) 'fb_page_post_stat'")
+     (SELECT MAX(updated) FROM fb_link_count) 'fb_link_count',
+     (SELECT MAX(updated_time) FROM fb_page_post) 'fb_page_post',
+     (SELECT MAX(updated) FROM fb_page_post_stat) 'fb_page_post_stat',
+     (SELECT MAX(imported) FROM tweets) 'tweets',
+     (SELECT MAX(imported) FROM ENsupportersActivities) 'EN_activities'")
   
   counts = result.first
 
   lastqueryrun << {:label => 'fb_link_count', :value => counts['fb_link_count']}
   lastqueryrun << {:label => 'fb_page_post', :value => counts['fb_page_post']}
   lastqueryrun << {:label => 'fb_page_post_stat', :value => counts['fb_page_post_stat']}
+  lastqueryrun << {:label => 'tweets', :value => counts['tweets']}
+  lastqueryrun << {:label => 'EN Activities', :value => counts['EN_activities']}
 
   send_event('DB_latest_query_runtime', { items: lastqueryrun })
 
 end
-
-# For a prettier formatting output
-=begin
-    USE test
-    SELECT
-     (SELECT CONVERT(VARCHAR, MAX(updated), 8) 'latest' FROM fb_link_count) 'fb_link_count',
-     (SELECT CONVERT(VARCHAR, MAX(updated_time), 8) 'latest' FROM fb_page_post) 'fb_page_post',
-     (SELECT CONVERT(VARCHAR, MAX(updated), 8) 'latest' FROM fb_page_post_stat) 'fb_page_post_stat'
-=end
