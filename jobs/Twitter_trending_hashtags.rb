2a33f80cd4e82d666f49a9393b1ab8385719cc72
gregology <greg@gho.st>
2013-05-30 17:09:33 -0400

    changed start time hashtag job

--- jobs/Twitter_trending_hashtags.rb
+++ jobs/Twitter_trending_hashtags.rb
@@ -4,7 +4,7 @@ require 'tiny_tds'
  
 yml = YAML::load(File.open('lib/db_settings.yml'))['prod_settings']
 
-SCHEDULER.every '60m', :first_in => 0 do |job|
+SCHEDULER.every '60m', :first_in => 39 do |job|
   twitter_trends = []
 
   client = TinyTds::Client.new(:username => yml['username'], :password => yml['password'], :host => yml['host'])
