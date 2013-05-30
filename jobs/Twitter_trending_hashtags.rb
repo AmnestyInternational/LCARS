88a3fee5d76da51a510de63e0c72c4612df30cca
Gregology <greg@gho.st>
2013-05-30 16:27:49 -0400

    Added non Amnesty human rights trending tweets

--- jobs/Twitter_trending_hashtags.rb
+++ jobs/Twitter_trending_hashtags.rb
@@ -19,6 +19,7 @@ SCHEDULER.every '60m', :first_in => 600 do |job|
     WHERE
       TA.term LIKE '#%' AND
       T.text LIKE '%Amnesty%International%' AND
+      T.created > DATEADD(DAY, -7, GETDATE()) AND
       TA.term NOT IN ('#Amnesty','#AmnestyInternational')
     GROUP BY TA.term
     ORDER BY COUNT(TA.term) DESC")
@@ -28,4 +29,29 @@ SCHEDULER.every '60m', :first_in => 600 do |job|
   end
 
   send_event('Twitter_trending_hashtags', { items: twitter_trends })
+
+
+  twitter_trends = []
+  result = client.execute("
+    USE externaldata
+    SELECT TOP 7 TA.term, COUNT(TA.term) 'Count'
+    FROM
+      tweets AS T
+      INNER JOIN
+      tweetsanatomize AS TA
+      ON T.id = TA.tweet_id
+    WHERE
+      TA.term LIKE '#%' AND
+      T.text NOT LIKE '%Amnesty%' AND
+      T.text LIKE '%human%rights%' AND
+      TA.term != '#humanrights' AND
+      T.created > DATEADD(DAY, -7, GETDATE())
+    GROUP BY TA.term
+    ORDER BY COUNT(TA.term) DESC")
+
+  result.each do |row|
+    twitter_trends << {:label=>row['term'], :value=>row['Count']}
+  end
+
+  send_event('Twitter_trending_hashtags_HR_but_non_Amnesty', { items: twitter_trends })
 end
