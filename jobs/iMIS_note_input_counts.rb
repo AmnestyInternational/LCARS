8cbb46cba21d16a9d6d66ea7e18c66edfbc39f8d
gregology <greg@gho.st>
2013-06-03 10:07:12 -0400

    changed imis notes job

--- jobs/iMIS_note_input_counts.rb
+++ jobs/iMIS_note_input_counts.rb
@@ -14,9 +14,14 @@ SCHEDULER.every '10m', :first_in => 115 do |job|
     FROM activity
     WHERE
       ACTIVITY_TYPE = 'NOTES' AND
-      TRANSACTION_DATE > DATEADD(DAY, -3, GETDATE()) AND
-      UF_3 != '' AND
-      UF_3 IN ('Aengus Bridgman','Jennifer Auten','Will Bryant','David Griffiths')
+      UF_3 IN ('Aengus Bridgman','Jennifer Auten','Will Bryant','David Griffiths') AND
+      SEQN >= (
+        SELECT TOP 1 SEQN
+        FROM Activity
+        WHERE
+         TRANSACTION_DATE >= DATEADD(DAY, -3, GETDATE()) AND
+         TRANSACTION_DATE <= GETDATE()
+        ORDER BY SEQN) -- This is the earliest Transaction Date
     GROUP BY UF_3
     ORDER BY COUNT('SEQN') DESC
     ")
