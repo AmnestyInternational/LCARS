a1f24bec3cbe28bda31911834a2a29be7622cef3
gregology <greg@gho.st>
2013-06-05 17:21:11 -0400

    moved intadash job

--- jobs/Twitter_Taksim_Square_Protests.rb
+++ jobs/Twitter_Taksim_Square_Protests.rb
@@ -2,6 +2,7 @@
 require 'yaml'
 require 'tiny_tds'
 require 'date'
+require 'instagram'
  
 yml = YAML::load(File.open('lib/db_settings.yml'))['prod_settings']
 
@@ -166,6 +167,27 @@ SCHEDULER.every '10m', :first_in => 262 do |job|
 end
 
 # http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=https://news.google.ca/news/feeds?q=Istanbul+protests&safe=off&cr=countryCA&bav=on.2,or.r_cp.r_qf.&bvm=bv.47244034,d.aWM&ion=1&biw=1440&bih=813&um=1&ie=UTF-8&output=rss
+
+
+
+# Instagram Client ID from http://instagram.com/developer
+Instagram.configure do |config|
+  config.client_id = '4936d8ef710d47d293df03642a8ea4ca' # move this a yml file
+end
+
+# Latitude, Longitude for location
+instadash_location_lat = '41.038333'
+instadash_location_long = '28.986944'
+
+SCHEDULER.every '10m', :first_in => 59 do |job|
+  photos = Instagram.media_search(instadash_location_lat,instadash_location_long)
+  if photos
+    photos.map! do |photo|
+      { photo: "#{photo.images.low_resolution.url}" }
+    end    
+  end
+  send_event('instadash', photos: photos)
+end
 
 
 
