7c6cd5e2e18d6e9e7b9e616ce506afcb8f7193f3
Gregology <greg@gho.st>
2013-06-10 16:06:19 -0400

    added ms volunteer counts

--- jobs/iMIS_note_input_counts.rb
+++ jobs/iMIS_note_input_counts.rb
@@ -14,7 +14,7 @@ SCHEDULER.every '10m', :first_in => 115 do |job|
     FROM activity
     WHERE
       ACTIVITY_TYPE = 'NOTES' AND
-      UF_3 IN ('Aengus Bridgman','Jennifer Auten','Will Bryant','David Griffiths') AND
+      UF_3 IN ('Aengus Bridgman','Jennifer Auten','Will Bryant','David Griffiths','Member Services Volunteer') AND
       SEQN >= (
         SELECT TOP 1 SEQN
         FROM Activity
@@ -31,5 +31,30 @@ SCHEDULER.every '10m', :first_in => 115 do |job|
   end
 
   send_event('iMIS_note_input_counts', { items: inputcounts })
+
+end
+
+SCHEDULER.every '5m', :first_in => 71 do |job|
+  msvinputcounts = []
+
+  result = client.execute("
+    SELECT UF_2 'Note_type', COUNT(SEQN) 'Count'
+    FROM Activity
+    WHERE
+      UF_3 = 'Member Services Volunteer' AND
+      SEQN >= (
+        SELECT MIN(SEQN)
+        FROM Activity
+        WHERE
+         TRANSACTION_DATE >= CAST(GETDATE() AS DATE) AND
+         TRANSACTION_DATE <= GETDATE())
+    GROUP BY UF_2
+    ")
+
+  result.each do |row|
+    msvinputcounts << {:label=>row['Note_type'], :value=>row['Count']}
+  end
+
+  send_event('iMIS_member_services_volunteer_counts', { items: msvinputcounts })
 
 end
